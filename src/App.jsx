c44b8217c6611ecd482a056331dc6b2fc37e50ed
Ricardo Alegria <deybiricardo3131@gmail.com>
2026-04-27 08:12:08 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,46 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 import AboutUs from "./components/AboutUs";
 import ProductList from "./components/ProductList";
 import CartItem from "./components/CartItem";
 
+const Navbar = () => {
+  const items = useSelector(state => state.cart.items);
+  const totalItems = items.reduce((acc, item) => acc + item.quantity, 0);
+
+  return (
+    <nav style={{ background: "#2d6a4f", padding: "10px" }}>
+      <Link to="/" style={{ color: "white", margin: "10px" }}>INICIO</Link>
+      <Link to="/plants" style={{ color: "white", margin: "10px" }}>PLANTAS</Link>
+      <Link to="/cart" style={{ color: "white", margin: "10px" }}>
+        CARRITO 🛒 ({totalItems})
+      </Link>
+    </nav>
+  );
+};
+
 const App = () => {
   return (
     <Router>
-      <div>
-        <nav style={{ background: "#2d6a4f", padding: "10px", color: "white" }}>
-          <Link to="/" style={{ margin: "10px", color: "white" }}>INICIO</Link>
-          <Link to="/plants" style={{ margin: "10px", color: "white" }}>PLANTAS</Link>
-          <Link to="/cart" style={{ margin: "10px", color: "white" }}>CARRITO</Link>
-        </nav>
+      <Navbar />
 
-        <Routes>
-          <Route path="/" element={
-            <div style={{ textAlign: "center", marginTop: "50px" }}>
-              <h1>PARADISE NURSERY 🌿</h1>
-              <Link to="/plants">
-                <button>COMENZAR</button>
-              </Link>
-            </div>
-          } />
+      <Routes>
+        <Route path="/" element={
+          <div style={{ textAlign: "center", marginTop: "50px" }}>
+            <h1>PARADISE NURSERY 🌿</h1>
+            <p>Las mejores plantas para tu hogar.</p>
+            <Link to="/plants">
+              <button>COMENZAR</button>
+            </Link>
+          </div>
+        } />
 
-          <Route path="/about" element={<AboutUs />} />
-          <Route path="/plants" element={<ProductList />} />
-          <Route path="/cart" element={<CartItem />} />
-        </Routes>
-      </div>
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/plants" element={<ProductList />} />
+        <Route path="/cart" element={<CartItem />} />
+      </Routes>
     </Router>
   );
 };
